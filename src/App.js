1593fbf3a0ff70ec76d9507a0e78e1f746742b45
sathwik227 <sathwik@jayeesha.com>
2020-10-06 01:20:27 +0530

    broken our homepage into smaller components and finished styling of the menu-items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import Homepage from './homepage.component'
 
-const App = () => {
+import HomePage from './pages/homepage/homepage.component';
+
+function App() {
   return (
     <div>
-      <Homepage />
+      <HomePage />
     </div>
   );
 }
